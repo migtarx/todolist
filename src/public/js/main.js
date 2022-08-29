2723977a184986386369df650e942e5ee74ae577
Miguel Puerta <miguel.puerta@biko2.com>
2022-08-29 09:31:21 +0200

    feat: Create task from the add task form modal

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,4 +1,4 @@
-function deleteTask (taskId){
+const deleteTask = (taskId) => {
     $.ajax({
         url: "/task/deleteTask",
         type: 'DELETE',
@@ -12,3 +12,26 @@ function deleteTask (taskId){
         }
     });
 }
+
+const createTask = (title, description) => {
+    $.ajax({
+        url: "/task/createTask",
+        type: 'POST',
+        contentType: 'application/json;charset=UTF-8',
+        data: JSON.stringify({'title':  title, 'description': description}),
+        success: function(){
+            return window.location.href = "/?success=1"
+        },
+        error: function(){
+            return window.location.href = "/?error=1"
+        }
+    });
+}
+$(function() {
+    $('#task-creation').on("submit", function(e) {
+      e.preventDefault();
+      const taskTitle = $('#task-title').val();
+      const taskDescription = $('#task-description').val();
+      createTask(taskTitle, taskDescription)
+    });
+  });
